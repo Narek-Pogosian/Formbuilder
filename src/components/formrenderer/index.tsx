3a27a5338450b7970ba7edb9a6c3d8d2b719b2bd
Narek Pogosian <narekpogosian2@gmail.com>
2024-07-31 09:20:37 +0200

    use react-hook-form with shadcn components for formrenderer

--- src/components/formrenderer/index.tsx
+++ src/components/formrenderer/index.tsx
@@ -5,40 +5,78 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { type FormSchema } from "@/lib/schemas/form-schema";
 import { useForm } from "react-hook-form";
 import { Button } from "../ui/button";
-import { Label } from "../ui/label";
 import { Input } from "../ui/input";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
 
 function FormRenderer({ form }: { form: FormSchema }) {
   const schema = createValidationSchema(form);
-  const {
-    handleSubmit,
-    register,
-    formState: { errors },
-  } = useForm<typeof schema>({
+  const f = useForm<typeof schema>({
     resolver: zodResolver(schema),
   });
 
   function onSubmit(data: typeof schema) {
     console.log(data);
   }
-  console.log(form);
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className="grid gap-8">
-      {form.map((field) => {
-        const label = field.label as keyof typeof schema;
 
-        return (
-          <div key={label}>
-            <Label htmlFor={label} className="capitalize">
-              {label}
-            </Label>
-            <Input id={label} type={field.type} {...register(label)} />
-            {errors[label] && <span>{errors[label]?.message?.toString()}</span>}
-          </div>
-        );
-      })}
-      <Button type="submit">Submit</Button>
-    </form>
+  return (
+    <Form {...f}>
+      <form onSubmit={f.handleSubmit(onSubmit)} className="grid gap-8">
+        {form.map((formField) => {
+          const label = formField.label as keyof typeof schema;
+
+          // OLD
+          // <div key={label}>
+          //   <Input id={label} type={formField.type} {...f.register(label)} />
+          // </div>;
+
+          if (formField.type === "text")
+            return (
+              <FormField
+                key={label}
+                control={f.control}
+                name={label}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{label}</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder=""
+                        {...field}
+                        value={field.value as string}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            );
+
+          if (formField.type === "number")
+            return (
+              <FormField
+                key={label}
+                control={f.control}
+                name={label}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{label}</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        min={formField.min}
+                        max={formField.max}
+                        placeholder=""
+                        {...field}
+                        value={field.value as number | string}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            );
+        })}
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }
 
