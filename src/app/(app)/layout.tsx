f2b885a2b43cbb3c57fa7ee6ff0350b6c560a130
Narek Pogosian <narekpogosian2@gmail.com>
2024-08-29 22:40:35 +0200

    add next top loader bar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,26 +3,30 @@ import { redirect } from "next/navigation";
 import { Toaster } from "@/components/ui/sonner";
 import SidebarNavigation from "./_components/sidebar-navigation";
 import MobileSidebar from "./_components/mobile-sidebar";
+import NextTopLoader from "nextjs-toploader";
 
 async function layout({ children }: Readonly<{ children: React.ReactNode }>) {
   const session = await getServerAuthSession();
   if (!session) throw redirect("/landing");
 
   return (
-    <div className="flex min-h-full">
-      <div className="shadow sticky top-0 z-50 hidden h-screen w-64 shrink-0 bg-background-card pr-2 lg:block">
-        <SidebarNavigation />
-      </div>
-      <div className="flex w-full flex-col">
-        <div className="p-4 lg:hidden">
-          <MobileSidebar />
+    <>
+      <NextTopLoader showSpinner={false} />
+      <div className="flex min-h-full">
+        <div className="shadow sticky top-0 hidden h-screen w-64 shrink-0 bg-background-card pr-2 lg:block">
+          <SidebarNavigation />
         </div>
-        <main className="relative grow px-4 lg:px-16 lg:py-8 xl:px-20">
-          {children}
-        </main>
+        <div className="flex w-full flex-col">
+          <div className="p-4 lg:hidden">
+            <MobileSidebar />
+          </div>
+          <main className="relative grow px-4 lg:px-16 lg:py-8 xl:px-20">
+            {children}
+          </main>
+        </div>
+        <Toaster position="top-center" duration={2000} />
       </div>
-      <Toaster position="top-center" duration={2000} />
-    </div>
+    </>
   );
 }
 
